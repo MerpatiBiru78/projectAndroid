bd42521f9652e4061fca4fc0b9d3451566e1e739
isacimanuelmumba <isacimanuelmumba@gmail.com>
2019-11-18 22:59:52 +0700

    update

--- projectAndroid/App.js
+++ projectAndroid/App.js
@@ -1,19 +1,76 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View , 
+  Dimensions, 
+  Image,
+  Button,
+  TouchableOpacity,
+  Switch,
+  Picker,
+  FlatList,
+} from 'react-native'; 
+//import ListProductScreen from './src/container/ListProductScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import LoginScreen from './LoginScreen';
+
+// import ListProductScreen from './src2/container/ListProductScreen';
+
+export default class App extends React.Component {
+  
+  state = {
+    isLogin: false,
+  }
+  
+  cetakHalo(param){
+    alert('HALOO');
+    console.log('HALOO');
+  }
+  
+  addPicker(){
+    for (i = 0; i < 3; i++) 
+    { 
+      return(
+        <Picker.Item label='agung'/>
+      )
+    }
+  }
+
+  login(){
+    alert('berhasil login')
+  }
+
+  render(){
+    return (
+      // <LoginScreen fungsiLogin={() => this.login()} username='test'></LoginScreen>
+
+      // <LoginScreen></LoginScreen>
+
+      //<ListProductScreen></ListProductScreen>
+
+      // <LoginScreen login={this.login}></LoginScreen>
+      
+      //<ListProductScreen></ListProductScreen>
+    );
+      
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection:'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  colorView: {
+    flex: 1, 
+    width: Dimensions.get('window').width, 
+    height: Dimensions.get('window').height,
+  },
+  textStyle: {
+    flex: 1,
+  }
 });
---
+++ projectAndroid/src/container/LoginScreen.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TextInput,
+  Dimensions,
+  TouchableOpacity,
+  Switch
+} from 'react-native';
+
+export default class LoginScreen extends React.Component{
+    
+    state = {
+        username: '',
+        password: '',
+        agreement: false,
+    }
+    
+    onLogin(){
+        alert(this.state.username)
+        // if(this.state.username === 'test' && this.state.password === '1234'){
+        //     const login = this.props.fungsiLogin;
+        //     // alert('berhasil login');
+        //     login();
+        // }
+        // else{
+        //     alert('gagal login');
+        // }
+    }
+    
+    render(){
+        return(
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.inputText}
+                    placeholder='username'
+                    placeholderTextColor='red'
+                    value={this.state.username}
+                    onChangeText={(username) => this.setState({username: username})}
+                ></TextInput> 
+                <TextInput
+                    style={styles.inputText}
+                    placeholder='password'
+                    placeholderTextColor='red'
+                    secureTextEntry={true}
+                    value={this.state.password}
+                    onChangeText={(password) => this.setState({password: password})}
+                ></TextInput> 
+
+                <View style={{flexDirection: 'row', marginTop: 10}}>
+                    <Switch
+                        value={this.state.agreement}
+                        onValueChange={(agreement) => this.setState({agreement: agreement})}
+                    ></Switch>
+                    <Text>Saya menyetujui syarat dan ketentuan</Text>
+                </View>
+
+                <TouchableOpacity
+                    style={styles.btnLogin}
+                    onPress={() => this.onLogin()}
+                    disabled={!this.state.agreement}
+                >
+                    <Text style={{color: 'white'}}>LOGIN</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection:'column',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputText: {
+        fontSize: 20,
+        width: Dimensions.get('window').width * 0.7,
+        borderBottomWidth: 1,
+    },
+    btnLogin: {
+        marginTop: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'blue',
+        width: Dimensions.get('window').width * 0.7,
+        height: Dimensions.get('window').height * 0.05,
+    },
+})
